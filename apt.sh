63032ed5827acabc28f2577daeb0e1a03a9d58ee
foxx3r <coluna123@protonmail.ch>
2019-05-05 13:40:37 -0300

    Update apt.sh
    Add suporte á UTF-8

--- apt.sh
+++ apt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #INTERPRETADOR QUE SERÁ UTILIZASO
 
+export LANG=C.UTF-8
 cp -r ./apt.sh /usr/bin/foxman #COPIANDO O ARQUIVO PARA OS BINARIOS PARA QUE VOCE POSSA EXECUTA-LO
 
 echo ' mmmmmm
@@ -8,31 +9,31 @@ echo ' mmmmmm
  #mmmmm #" "#   #m#   # # #  "   #  #"  #
  #      #   #   m#m   # # #  m"""#  #   #
  #      "#m#"  m" "m  # # #  "mm"#  #   # '
-echo "agora voce pode executar este programa apenas digitando 'foxman'!!!"
+echo 'agora você pode executar este programa apenas digitando "foxman"!!!'
 echo "Telegram: @Foxxer_SA"
-echo "Se inscreve la '@AcervoHackerBR'!!!"
+echo "Se inscreve lá '@AcervoHackerBR'!!!"
 echo "GitHub: foxx3r"
 sleep 1 #ESPERAR 1 SEGUNDO
 
-while true #LOOP INFINITO ENQUANTO A CONDICAO FOR TRUE= ENQUANTO
+while true #LOOP INFINITO ENQUANTO A CONDIÇÃO FOR TRUE, WHILE = ENQUANTO
 do
 
 echo "--------------------------------------------"
-echo "[1] ATUALIZAR REPOSITORIOS                 |"
+echo "[1] ATUALIZAR REPOSITÓRIOS                 |"
 echo "[2] ATUALIZAR PACOTES                      |"
 echo "[3] INSTALAR UM PACOTE                     |"
 echo "[4] REMOVER UM PACOTE                      |"
 echo "[5] PESQUISAR POR UM PACOTE                |"
-echo "[6] AUTOREMOVER PACOTES DESNECESSARIOS     |"
+echo "[6] AUTO-REMOVER PACOTES DESNECESSÁRIOS    |"
 echo "--------------------------------------------"
-read -p "Selecione uma opcao -> " fox #PEÇA PARA O USUARIO DIGITAR UM VALOR, ESTE VALOR SERA ARMAZENADO DENTRO DA VARIAVEL $fox
+read -p "Selecione uma opção -> " fox #PEÇA PARA O USUÁRIO DIGITAR UM VALOR, ESTE VALOR SERÁ ARMAZENADO DENTRO DA VARIÁVEL $fox
 
 #SE
-if [ $fox == 1 ] 
+if [ $fox == 1 ] #SE A VARIÁVEL fox for IGUAL Á 1 EXECUTE O BLOCO DE COMANDOS ABAIXO
 then
     echo "atualizando..."
     sudo apt update > /dev/null 
-    echo "repositorios atualizados com sucesso!"
+    echo "repositórios atualizados com sucesso!"
 
 elif [ $fox == 2 ]
 then
@@ -42,21 +43,21 @@ then
 
 elif [ $fox == 3 ]
 then
-    read -p "Qual pacote deseja instalar? -> " fux
+    read -p "Qual pacote você deseja instalar? -> " fux
     echo "instalando..."
     sudo apt install $fux -y > /dev/null
     echo "pacote instalado com sucesso!"
 
 elif [ $fox == 4 ]
 then
-    read -p "Qual pacote voce deseja remover? -> " fix
+    read -p "Qual pacote você deseja remover? -> " fix
     echo "removendo pacote..."
     sudo apt purge $fix -y > /dev/null
     echo "pacote removido com sucesso!"
 
 elif [ $fox == 5 ]
 then
-    read -p "Qual pacote deseja procurar? -> " fax
+    read -p "Qual pacote você deseja procurar? -> " fax
     echo "Pressione a tecla Q para sair"
     sleep 2
     apt-cache search $fax | less
